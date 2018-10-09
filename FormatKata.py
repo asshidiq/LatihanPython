c90502b8510c2b3ed5044a9a4b88ee4abe0a63d1
Adam Asshidiq <a.asshidiq@gmail.com>
2018-10-09 11:08:34 +0700

    Latihan import module dan function

---
+++ FormatKata.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def kapital(data):
+    return data.upper()
+
+def kecil(data):
+    return data.lower()
+
+def kalimat(data):
+    return data.capitalize()
+
+def judul(data):
+    return data.title()
+
+isi = 'test sample'
+isi2 = 'TEST'
+
+print(kapital(isi))
+print(kecil(isi2))
+print(kalimat(isi))
+print(judul(isi))
+
+
+# In[ ]:
+
+
+
+
