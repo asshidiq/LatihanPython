65bdc9ed370d8631eecb33f693fbf400de74157b
Adam Asshidiq <a.asshidiq@gmail.com>
2018-10-09 11:42:00 +0700

    Latihan membuat dan import package: latihan

---
+++ training/module1.py
@@ -0,0 +1,5 @@
+def perkalian(x,y):
+    return x*y
+
+def pembagian(x, y=1):
+    return x/y
---
+++ training/module2.py
@@ -0,0 +1,7 @@
+from math import sqrt
+
+def kuadrat(x):
+    return x*x
+
+def akar(x):
+    return sqrt(x)
